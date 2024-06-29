8cf2d1120d82877e87466567c0ba32b46fce2735
pineaulo <pineaulo@etu.utc.fr>
2024-06-29 18:58:08 +0200

    Fix sliders

--- public_html/scripts/slider.js
+++ public_html/scripts/slider.js
@@ -1,38 +1,35 @@
-document.querySelectorAll('.slider').forEach((slider) => {
+var globalSlideCounter = 0;
+
+const updators = [...document.querySelectorAll('.slider')].map((slider) => {
     const slides = slider.querySelectorAll('.slide');
     const prevArrow = slider.querySelector('.slider-arrow-left');
     const nextArrow = slider.querySelector('.slider-arrow-right');
-    
-    let currentSlide = 0;
-    
-    function updateSlide() {
-      slides.forEach((slide) => {
-        slide.classList.remove('active');
-      });
-      slides[currentSlide].classList.add('active');
-    }
-    
-    function nextSlide() {
-      if (currentSlide === slides.length - 1) {
-        currentSlide = 0;
-      } else {
-        currentSlide++;
-      }
-      updateSlide();
-    }
-    
-    function prevSlide() {
-      if (currentSlide === 0) {
-        currentSlide = slides.length - 1;
-      } else {
-        currentSlide--;
-      }
-      updateSlide();
-    }
-    
+
     nextArrow?.addEventListener('click', nextSlide);
     prevArrow?.addEventListener('click', prevSlide);
-    
-    setInterval(nextSlide, 10000);
+    return () => {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });
+        let currentSlide = (slides.length + (globalSlideCounter % slides.length)) % slides.length;
+        slides[currentSlide].classList.add('active');
+    };
 });
+
+function updateSliders() {
+    updators.forEach(updator => updator());
+}
+
+function nextSlide() {
+    globalSlideCounter++;
+    updateSliders();
+}
+
+function prevSlide() {
+    globalSlideCounter--;
+    updateSliders();
+}
+if(updators.length > 0) {
+    setInterval(nextSlide, 10000);
+}
 
