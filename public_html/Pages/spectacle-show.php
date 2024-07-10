05661d0fb2228f462a1590313bab77666ead329b
Maxime Claudel <maxime.claudel54@gmail.com>
2024-07-10 22:47:35 +0200

    Les_aventure_de_maxou

--- public_html/Pages/spectacle-show.php
+++ public_html/Pages/spectacle-show.php
@@ -4,19 +4,25 @@ page_header("Spectacles / Show");
 ?>
 <section>
     <article>
-        <h2>Spectacle destiné aux enfants</h2>
-        <p>Les aventures d’Henry.
-            Henry un jeune de 18 ans recherche l’aventure, il tombe nez à nez face à un magicien s’appelant Merlin
-            et l’envoya dans un livre
-            pour qu’il puissent vivre une aventure inoubliable. <br>
-            A vous d’aider au mieux Henry.
-        </p>
+        <h2>LES AVENTURES DE MAXOU</h2>
+        <p>Maxou, un jeune magicien, recherche son fidèle compagnon qui a disparu. Voyagez à travers divers lieux pour retrouver son fidèle « Pampan ». Un spectacle interactif idéal pour les enfants de tous âges. Inclus dans le spectacle, un retour sur la légende de Saint Nicolas dans une version inédite. </p>
+
+</p>
         <br> 
-        <img src="/Image/image temporaire.jpg"> <br> <!-- Pourquoi pas mettre un slider-->
+        <div class="slider">
+            <img src="/Image/slider_spectacle_les_aventures_de_maxou/solo.jpg" alt="Slide 1" class="active slide">
+            <img src="/Image/slider_spectacle_les_aventures_de_maxou/un.jpg" alt="Slide 2" class="slide">
+            <img src="/Image/slider_spectacle_les_aventures_de_maxou/deux.jpg" alt="Slide 3" class="slide">
+            <img src="/Image/slider_spectacle_les_aventures_de_maxou/trois.jpg" alt="Slide 4" class="slide">
+            <div class="slider-arrow slider-arrow-left">&lt;</div>
+            <div class="slider-arrow slider-arrow-right">&gt;</div>
+        </div> <br>
         <p>
-            Durée de la représentation : 1h30 <br>
-            Adaptation de l’âges possible (sur demande) <br>
-            Capacité de 200 personnes.
+        Nous recommandons ce spectacle pour une salle pouvant accueillir de 50 à 200 personnes, pour une durée d’environ 1h30. <br> La prestation de régie n’est pas incluse mais est disponible en option. <br> Pour toute information technique, veuillez nous contacter. </p> <br>
+        <!-- Pourquoi pas mettre un bouton contact -->
+        <p>
+        <br>Afin de nous adapter au mieux à vos exigences et à vos budgets, nous vous proposons plusieurs options : <br> - L’arrivée du Père Noël <br> - L’arrivée de Saint Nicolas <br>- La prestation de régie
+    
         </p>
     </article> <!-- (j'ai un doute de laisse ceci)
     <article>
