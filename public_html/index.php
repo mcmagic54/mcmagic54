2fe0c1e65cde0d47653056247c5de246cc76e33d
Maxime Claudel <maxime.claudel54@gmail.com>
2024-12-11 22:45:22 +0100

    indexcomentaire

--- public_html/index.php
+++ public_html/index.php
@@ -1,45 +1,71 @@
 <?php
+// Inclusion des éléments récurrents comme l'en-tête via un fichier PHP
 include("../private_html/php/elements.php");
+// Fonction personnalisée pour générer l'en-tête de la page avec le titre "Accueil"
 page_header("Accueil");
 ?>
+
+<!-- Section principale de la page d'accueil -->
 <section class="accueil">
+    <!-- Ajout d'une bordure noire pour styliser la section principale -->
     <div class="bordure_noir">
+        <!-- Slider principal pour le texte d'accueil -->
         <div class="slider accueil-text">
+            <!-- Première diapositive du slider -->
             <div class="active slide">Bienvenue sur mon site ceci est un test pour la détructions</div>
+            <!-- Deuxième diapositive du slider -->
             <div class="slide">Un magicien près de vous mais pas que iiiiiiiiiiii ...</div>
-            <div class="slide">Collectivité, professionelle, Annviersaires...</div>
+            <!-- Troisième diapositive du slider -->
+            <div class="slide">Collectivité, professionnelle, Anniversaires...</div>
         </div>
+
+        <!-- Slider pour les images d'accueil -->
         <div class="slider accueil-img">
+            <!-- Image 1 du slider avec une classe active pour la rendre visible par défaut -->
             <img src="/Image/Slider_principal/slide1.jpg" alt="Slide 1" class="active slide">
+            <!-- Image 2 du slider -->
             <img src="/Image/Slider_principal/slide2.jpg" alt="Slide 2" class="slide">
+            <!-- Image 3 du slider -->
             <img src="/Image/Slider_principal/slide3.jpg" alt="Slide 3" class="slide">
+            <!-- Flèches de navigation pour le slider -->
             <div class="slider-arrow slider-arrow-left">&lt;</div>
             <div class="slider-arrow slider-arrow-right">&gt;</div>
         </div>
+
+        <!-- Slider secondaire pour plus de texte descriptif -->
         <div class="slider accueil-text">
-            <div class="active slide">Bonjour, je me présente. Je suis Maxime CLAUDEL, magicien à votre service Bonjour, je me présente. Je suis Maxime CLAUDEL, magicien à votre service</div>
+            <div class="active slide">Bonjour, je me présente. Je suis Maxime CLAUDEL, magicien à votre service</div>
             <div class="slide">Un magicien pour de la magie rapprochée, pensez au close-up</div>
             <div class="slide">Découvrez la magie et ses spécialités (cartes, sculpture de ballon, magie classique)</div>
         </div>
     </div>
 </section>
+
+<!-- Inclusion des icônes ou liens vers les réseaux sociaux -->
 <?php reseaux_sociaux(false, true); ?>
+
+<!-- Section avec une démonstration vidéo intégrée -->
 <section class="dark">
     <article class="center">
+        <!-- Intégration d'une vidéo YouTube via iframe -->
         <iframe title="Démonstration" src="https://www.youtube.com/embed/bkrZmeuGMiM" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </article>
 </section>
 
-<h1 class="fontslidelogo">- - - Il nous ont fait confiance - - -</h1>
+<!-- Titre de la section des clients ayant fait confiance au magicien -->
+<h1 class="fontslidelogo">- - - Ils nous ont fait confiance - - -</h1>
 
+<!-- Section listant les logos des clients dans un slider horizontal -->
 <section class="Nous ont fait confiance">
     <div class="Slider-client">
+        <!-- Liste des logos avec une animation en défilement horizontal (marquee) -->
         <ul class="marquee-content">
-        <li><img src="/Image/sliderclientpart/o2.jpg" class="o2"></li>
-        <li><img src="/Image/sliderclientpart/nmeinternational.jpg" class="NMEinternational"></li>
-        <li><img src="/Image/sliderclientpart/logomarangesilv.jpg" class="Mairie_Marange-silvange"></li>
+            <li><img src="/Image/sliderclientpart/o2.jpg" alt="Logo O2" class="o2"></li>
+            <li><img src="/Image/sliderclientpart/nmeinternational.jpg" alt="Logo NME International" class="NMEinternational"></li>
+            <li><img src="/Image/sliderclientpart/logomarangesilv.jpg" alt="Logo Mairie Marange-Silvange" class="Mairie_Marange-silvange"></li>
         </ul>
     </div>
- </section>
+</section>
 
+<!-- Inclusion du pied de page via une fonction PHP personnalisée -->
 <?php page_end(); ?>
