17b3f153a4ddf2428757efb373e8b7a0d708bfc4
Maxime Claudel <maxime.claudel54@gmail.com>
2024-12-24 00:47:00 +0100

    chargementavisgoogle

---
+++ public_html/Pages/testapiavisgoogle.php
@@ -0,0 +1,39 @@
+<?php
+include ("../../private_html/php/elements.php");
+page_header("Structure en Ballons");
+?>
+<section>
+    <article>
+        <h2>Location de Structures en Ballons – Transformez vos Événements !</h2>
+        <p>Faites de vos événements des moments inoubliables avec nos structures en ballons spectaculaires ! Que ce soit pour des fêtes d'anniversaire, des mariages, ou des événements corporatifs, nous avons des solutions sur mesure pour vous. <br> </p>
+    </article>
+    <article>
+        <h2>Les différentes arches</h2> <br>
+        <h3>Arches Standard : </h3> 
+
+        <img src="/Image/image temporaire.jpg"> <br>
+
+        <p> L'arches de ballons idéale pour des entrées majestueuses. L'arche s'adapte parfaitement à tous les endroits. Nous disposons d'une pallettes de couleurs complétes pour s'ajuster au mieux à vos exigences et à vos événements. Le temps d'instalations étant 1h30-2h. </p> <br>
+        <h3> Arches Prenuim : </h3>
+        <img src="/Image/arches-prenuim.jpg"> <br>
+
+        <p> L'arches de ballons idéale pour décorée un intérieurs ou pour une prise de photos pour un mariage. Nous disposons d'une pallettes de couleurs complétes pour s'ajuster au mieux à vos exigences et à vos événements. Le temps d'instalations étant 2h30-3h. </p> <br>
+
+        <h3> Nous réalisons des prestations sur sur mesure - Contactez-nous pour un devis personnalisé - </h3>
+
+        <br><p> bouton contact</p> <br>
+
+        <h2>Les autres structures</h3>
+
+        <h3> Support de ballons</h3>
+
+        <img src="/Image/image temporaire.jpg"> <br>
+
+        <h3> Les structures en chiffres</h3>
+
+        <img src="/Image/image temporaire.jpg"> <br>
+
+
+    </article>
+</section>
+<?php page_footer(); ?>
